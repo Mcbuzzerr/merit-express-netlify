a24e82c8a804c96e6c125f43288c2e5a962f6187
Mcbuzzerr <mcbuzzer@gmail.com>
2020-09-23 10:48:47 -0600

    added SCSS

--- express/routes.js
+++ express/routes.js
@@ -1,7 +1,8 @@
 'use strict';
 const routes = [
   { route: "/", file: "index.html" },
-  { route: "/away", file: "away.html" }
+  { route: "/away", file: "away.html" },
+  { route: "/website", file: "website.html" }
 ]
 
 module.exports = routes;
